e724f886f432d8df32afd17ddb4975cfaa5f74c3
Yilei Yang <yileiyang@google.com>
2021-06-23 15:43:32 -1000

    Directly use the Python 3 tempfile.TemporaryDirectory since this no longer supports Python 2.

--- moonlight/glyphs/saved_classifier_test.py
+++ moonlight/glyphs/saved_classifier_test.py
@@ -18,8 +18,8 @@ from __future__ import division
 from __future__ import print_function
 
 import os.path
+import tempfile
 
-from backports import tempfile
 import numpy as np
 import tensorflow as tf
 
